ef1d92e86399b727ae2a06ef2c732a6893eea699
jasonpanosso <jasonpanosso@protonmail.com>
2025-06-08 19:32:54 -0400

    fix: properly setup crowdsec secrets(bouncer key)

--- terraform/bitwarden_secrets/crowdsec.tf
+++ terraform/bitwarden_secrets/crowdsec.tf
@@ -21,3 +21,15 @@ resource "bitwarden_secret" "crowdsec_registration_token" {
   project_id = bitwarden_project.this.id
   note       = "Crowdsec registration api token"
 }
+
+resource "random_password" "crowdsec_bouncer_key_traefik" {
+  length  = 64
+  special = false
+}
+
+resource "bitwarden_secret" "crowdsec_bouncer_key_traefik" {
+  key        = "CROWDSEC_BOUNCER_KEY_TRAEFIK"
+  value      = random_password.crowdsec_bouncer_key_traefik.result
+  project_id = bitwarden_project.this.id
+  note       = "Crowdsec API key for Traefik bouncer"
+}
