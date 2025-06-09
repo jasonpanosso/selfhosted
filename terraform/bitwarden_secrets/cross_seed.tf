2a478848c13e0198bb6e562b2f4cecd334034c8e
jasonpanosso <jasonpanosso@protonmail.com>
2025-06-09 11:38:07 -0400

    feat: add cross-seed

---
+++ terraform/bitwarden_secrets/cross_seed.tf
@@ -0,0 +1,11 @@
+resource "random_password" "cross_seed_api_token" {
+  length  = 128
+  special = true
+}
+
+resource "bitwarden_secret" "cross_seed_api_token" {
+  key        = "CROSS_SEED_API_TOKEN"
+  value      = random_password.cross_seed_api_token.result
+  project_id = bitwarden_project.this.id
+  note       = "cross-seed api token"
+}
