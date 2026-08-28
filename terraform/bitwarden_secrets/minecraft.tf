69468eb5498bdd3a14406dc01758f45e85c160a4
jasonpanosso <jasonpanosso@protonmail.com>
2026-08-28 15:07:28 -0400

    feat: setup minecraft proxy

--- terraform/bitwarden_secrets/minecraft.tf
+++ terraform/bitwarden_secrets/minecraft.tf
@@ -3,6 +3,18 @@ resource "random_password" "minecraft_cobblemon_rcon_password" {
   special = false
 }
 
+resource "random_password" "minecraft_proxy_secret" {
+  length  = 64
+  special = false
+}
+
+resource "bitwarden-secrets_secret" "minecraft_proxy_secret" {
+  key        = "MINECRAFT_PROXY_SECRET"
+  value      = random_password.minecraft_proxy_secret.result
+  project_id = bitwarden_project.this.id
+  note       = "Minecraft proxy encryption secret"
+}
+
 resource "bitwarden-secrets_secret" "minecraft_cobblemon_rcon_password" {
   key        = "MINECRAFT_COBBLEMON_RCON_PASSWORD"
   value      = random_password.minecraft_cobblemon_rcon_password.result
